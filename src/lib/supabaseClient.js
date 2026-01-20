17bdca4b16e2aa91b81f12bb9b60ef2e77b6cfba
stephenjohngh <stephenjohn.email@gmail.com>
2026-01-20 09:16:44 +0000

    Update supabaseClient.js

--- src/lib/supabaseClient.js
+++ src/lib/supabaseClient.js
@@ -1,4 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
-import { PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY } from '$env/static/public'
+//import { PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY } from '$env/static/public'
+
+const PUBLIC_SUPABASE_URL = import.meta.env.NF_SUPABASE_URL
+const PUBLIC_SUPABASE_ANON_KEY = import.meta.env.NF_SUPABASE_ANON_KEY
 
 export const supabase = createClient(PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY)
