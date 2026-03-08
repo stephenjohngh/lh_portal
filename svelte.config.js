ad5edd3d2a24db19a6080d36d05c4364f365b94e
sp <sp@sp.co>
2026-03-08 22:22:15 +0000

    v9

--- svelte.config.js
+++ svelte.config.js
@@ -1,21 +1,23 @@
-
 import adapterNetlify from '@sveltejs/adapter-netlify';
 import adapterNode from '@sveltejs/adapter-node';
-
-const adapter = process.env.DEPLOYMENT_TARGET === 'northflank' 
-  ? adapterNode() 
-  : adapterNetlify();
-
-
-
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
-export default {
+// 1. Select the function (do NOT add () here)
+const selectedAdapter = process.env.DEPLOYMENT_TARGET === 'northflank' 
+  ? adapterNode 
+  : adapterNetlify;
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
   preprocess: vitePreprocess(),
   kit: {
-    adapter: adapter({
-      edge: false,
-      split: false
-    })
+    // 2. Execute the selected adapter with conditional options
+    adapter: selectedAdapter(
+      process.env.DEPLOYMENT_TARGET === 'northflank' 
+        ? { out: 'build' } // Node options
+        : { edge: false, split: false } // Netlify options
+    )
   }
 };
+
+export default config;
